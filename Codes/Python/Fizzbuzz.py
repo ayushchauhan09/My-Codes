a15cbd6816553ed1835d59c6cbec8eec2b75b28e
Ayush Chauhan <77163105+ayushchauhan09@users.noreply.github.com>
2021-02-21 13:52:04 +0530

    Update Fizzbuzz.py

--- Codes/Python/Fizzbuzz.py
+++ Codes/Python/Fizzbuzz.py
@@ -1,12 +1,9 @@
-i=1
-while i <= 100:
-    if i%3==0:
-        print("Fizz", end="")
-        if i%5==0:
-            print("Buzz", end="")
+for i in range(1,101):
+    if i%15==0:
+        print('FizzBuzz')
+    elif i%3==0:
+        print('Fizz')
     elif i%5==0:
-        print("Buzz", end="")
+        print('Buzz')
     else:
-        print(i, end="")
-    print()
-    i+=1
+        print(i)
