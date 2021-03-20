77d056fc0a261ef94108c13ed60ada21936fea0a
Ayush Chauhan <77163105+ayushchauhan09@users.noreply.github.com>
2021-03-20 22:38:57 +0530

    Add files via upload

---
+++ Codes/Python/Helping-Chef.py
@@ -0,0 +1,7 @@
+T = int(input())
+for _ in range(T):
+    a = int(input())
+    if a<10:
+        print("Thanks for helping Chef!")
+    else:
+        print(-1)
---
+++ Codes/Python/Lucky-Four.py
@@ -0,0 +1,5 @@
+T = int(input())
+for _ in range(T):
+    a = int(input())
+    a = str(a)
+    print(a.count("4"))
